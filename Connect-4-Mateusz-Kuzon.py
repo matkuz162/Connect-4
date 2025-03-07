5492501594f93f83513bf03c9d94bc751a0fa881
matkuz162 <kuzonmateusz60@gmail.com>
2025-03-07 15:18:21 +0000

    place

--- Connect-4-Mateusz-Kuzon.py
+++ Connect-4-Mateusz-Kuzon.py
@@ -1,10 +1,49 @@
 import random
+import os
+os.system('color')
+
+red='\33[31m'
+yellow='\33[33m'
+default='\033[0m'
 
 def display(board):
+    
     for row in board:
         print(" | ".join(row))
-        print("-"*9)
+        print("-"*25)
+    print("0 | 1 | 2 | 3 | 4 | 5 | 6")
 
-board = [[" " for _ in range(3)] for _ in range(3)]
+#def check_winner(board,player):
 
-display(board)
+
+def is_full(board):
+  return all(board[row][col]!=" " for row in range(7) for col in range(6))
+
+
+def play_game():
+    board = [[" " for _ in range(7)] for _ in range(6)]
+    display(board)
+
+    while True:
+        try:
+            col = int(input("Enter your move (0-6): "))
+            if board[0][col] == " ":
+                board[0][col] = red+u'\u25CF'+default
+                break
+            else:
+                print("Column Full. Try again.")
+        except (ValueError, IndexError):
+             os.system('cls||clear')
+             display(board)
+             print("Invalid input. Enter column numbers between 0 and 6.")
+    os.system('cls||clear')
+    display(board)
+
+
+
+play_game()
+
+
+
+#print(red+u'\u25CF')
+#print(yellow+u'\u25CF')
