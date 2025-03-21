750e206564739d8c57daee39e235852c65f1d973
matkuz162 <kuzonmateusz60@gmail.com>
2025-03-21 13:18:34 +0000

    random agent

--- Connect-4-Mateusz-Kuzon.py
+++ Connect-4-Mateusz-Kuzon.py
@@ -22,31 +22,64 @@ def is_full(board):
   return all(board[row][col]!=" " for row in range(7) for col in range(6))
 
 def random_agent_move(board):
-  empty_cells = [(row,col) for row in range(7)for col in range(6) if board[0][col] ==  " " ]
-  return random.choice(empty_cells)
+  random_cell = random.randint(0,6)
+  while True:
+    if board[0][random_cell] == " ":
+        row = range(5,0,-1)
+        for i in row:
+            if board[i][random_cell] == " ":
+                board[i][random_cell] = yellow+u'\u25CF'+default
+                display(board)
+                print(f"Random Agent placed at ({random_cell}).")
+                break
+        break
+    else:
+        random_cell = random.randint(0,6)
+
 
 def play_game():
     board = [[" " for _ in range(7)] for _ in range(6)]
     display(board)
 
     while True:
-        try:
-            col = int(input("Enter your move (0-6): "))
-            if board[0][col] == " ":
-                row = range(5,0,-1)
-                for i in row:
-                    if board[i][col] == " ":
-                        board[i][col] = red+u'\u25CF'+default
-                        break
-                break
-            else:
-                print("Column Full. Try again.")
-        except (ValueError, IndexError):
-             os.system('cls||clear')
-             display(board)
-             print("Invalid input. Enter column numbers between 0 and 6.")
-    os.system('cls||clear')
-    display(board)
+        while True:
+            try:
+                col = int(input("Enter your move (0-6): "))
+                if board[0][col] == " ":
+                    row = range(5,0,-1)
+                    for i in row:
+                        if board[i][col] == " ":
+                            board[i][col] = red+u'\u25CF'+default
+                            break
+                    break
+                else:
+                    print("Column Full. Try again.")
+            except (ValueError, IndexError):
+                os.system('cls||clear')
+                display(board)
+                print("Invalid input. Enter column numbers between 0 and 6.")
+
+        os.system('cls||clear')
+        display(board)
+
+        #check winner
+
+        if is_full(board):
+            print("The game resulted in a draw")
+            break
+
+
+        #random agent
+        random_agent_move()
+
+        #check winner
+
+        if is_full(board):
+            print("The game resulted in a draw")
+            break
+
+
+
 
 
 
