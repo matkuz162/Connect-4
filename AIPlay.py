55e68b553b5073f21c639f86c435ee7a016dd869
matkuz162 <kuzonmateusz60@gmail.com>
2025-05-09 06:51:27 +0100

    ml vs mm

--- AIPlay.py
+++ AIPlay.py
@@ -6,6 +6,9 @@ import numpy as np
 from Agents.RandomAgent import random_agent_move
 from Agents.SmartAgent import smart_agent_move
 from Agents.MiniMaxAgent import Minimax_Agent
+from Agents.MachineLearningAgent import MachineLearningAgent
+
+
 
 def random_vs_smart(display_function, check_winner_function, is_full_function):
     random_wins = 0
@@ -93,5 +96,52 @@ def smart_vs_minimax(display_function, check_winner_function, is_full_function):
     print(f"The game ended in a draw: {draws} times.")
 
 
-def ml_vs_minimax():
-    print("not implemented")
+def ml_vs_minimax(display_function, check_winner_function, is_full_function):
+    ml_wins = 0
+    minimax_wins = 0
+    draws = 0
+    os.system('cls||clear')
+    print("Loading...")
+
+    ml_agent = MachineLearningAgent(player_symbol='●')
+    minimax_agent = Minimax_Agent(depth=4)
+
+    for game in range(0, 10):
+        
+        board = [[" " for _ in range(7)] for _ in range(6)]
+
+        while True:
+            col = ml_agent.choose_move(board)
+            for row in range(5,-1,-1):
+                if board[row][col] == " ":
+                    board[row][col] = '●'
+                    break
+
+            if check_winner_function(board, '●'):
+                ml_wins += 1
+                break
+
+            if is_full_function(board):
+                draws += 1
+                break
+
+
+            minimax_agent = Minimax_Agent(depth=4)
+            col = minimax_agent.best_move(board, check_winner_function, is_full_function)
+            for row in range(5,-1,-1):
+                if board[row][col] == " ":
+                    board[row][col] = '○'
+                    break
+
+            if check_winner_function(board, '○'):
+                minimax_wins += 1
+                break
+
+            if is_full_function(board):
+                draws += 1
+                break
+
+    os.system('cls||clear')
+    print(f"Machine Learning Agent won: {ml_wins} times.")
+    print(f"Mini-Max Agent won: {minimax_wins} times.")
+    print(f"The game ended in a draw: {draws} times.")
